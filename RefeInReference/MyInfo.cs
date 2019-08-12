ee1f1abdfa2bb0855cefcfb07ab5610aeba2e02b
liupenghao <haochen214@163.com>
2019-08-12 21:47:42 +0800

    按引用传递引用类型

---
+++ RefeInReference/MyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefeInReference
+{
+    class MyInfo
+    {
+        public string MyName;
+        public int MyAge;
+
+        public MyInfo(string name, int age)
+        {
+            MyName = name;
+            MyAge = age;
+        }
+
+        public MyInfo()
+        {
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("{0}的年龄是{1}", MyName, MyAge);
+        }
+    }
+}
--- RefeInReference/Program.cs
+++ RefeInReference/Program.cs
@@ -4,31 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ValueInReference
+namespace RefeInReference
 {
     class Program
     {
         /// <summary>
-        /// 按值传递引用类型
+        /// 按引用传递引用类型
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("输出用户的年龄信息：");
-            MyInfo info = new MyInfo("放大为", 24);
+            MyInfo info = new MyInfo("房大为", 55);
             Console.WriteLine("未改变用户信息前");
             info.PrintInfo();
-            SendAPersonByValue(info);
+            SendAPersonByValue(ref info);
             Console.WriteLine("改变后的用户年龄信息：");
             info.PrintInfo();
             Console.ReadLine();
         }
 
-
-        public static void SendAPersonByValue(MyInfo info)
+        public static void SendAPersonByValue(ref MyInfo info)
         {
-            info.MyAge = 99;
-            info = new MyInfo("顾丽丽", 99);
+            info.MyAge = 25;
+            info = new MyInfo("顾丽丽", 25);
         }
     }
 }
