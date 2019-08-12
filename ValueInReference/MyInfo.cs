f504bf491b7270a8cd3bddd4f295072d2f8ac55d
liupenghao <haochen214@163.com>
2019-08-12 21:40:38 +0800

    按值传递引用类型

---
+++ ValueInReference/MyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueInReference
+{
+    class MyInfo
+    {
+        public string MyName;
+        public int MyAge;
+
+        public MyInfo(string name ,int age)
+        {
+            MyName = name;
+            MyAge = age;
+        }
+
+        public MyInfo()
+        {
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("{0}的年龄是{1}", MyName, MyAge);
+        }
+
+       
+    }
+}
---
+++ ValueInReference/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueInReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输出用户的年龄信息：");
+            MyInfo info = new MyInfo("放大为", 24);
+            Console.WriteLine("未改变用户信息前");
+            info.PrintInfo();
+            SendAPersonByValue(info);
+            Console.WriteLine("改变后的用户年龄信息：");
+            info.PrintInfo();
+            Console.ReadLine();
+        }
+
+
+        public static void SendAPersonByValue(MyInfo info)
+        {
+            info.MyAge = 99;
+            info = new MyInfo("顾丽丽", 99);
+        }
+    }
+}
